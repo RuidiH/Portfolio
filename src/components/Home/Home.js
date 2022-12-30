beccd68655753d3823a5d457701d401295f3f84e
RuidiH <764342051>
2022-12-30 13:33:48 -0800

    mmmmm

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 const Home = () => {
-    return (
-    <section className='section-home'>
+  return (
+    <section className="section-home">
+      <div className="section-home__background">
         <h1 className="section-home__heading-large">Welcome To</h1>
-        <h2 className="section-home__heading-medium">My Home Page</h2>
+        <h2 className="section-home__heading-medium">The Home Page of</h2>
+      </div>
     </section>
-    );
+  );
 };
 
 export default Home;
